96edb08f98d9c05380e782d15bcc7b6bf19eec1b
mahithsc <mahith.chitrapu@gmail.com>
2022-05-03 18:30:56 -0400

    creating an api which appends todos to the list

--- server.py
+++ server.py
@@ -2,13 +2,16 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return ('hello world')
+check_list = []
 
-@app.route('/<string:name>')
-def name(name):
-    return (name)
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return jsonify(check_list)
+    if request.method == 'POST':
+        request_data = request.get_json()
+        check_list.append(request_data)
+        return jsonify(check_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
