e16b775b524a42da3b7e6c04c9dcc9701e9d1528
mahithsc <mahith.chitrapu@gmail.com>
2022-05-03 18:07:18 -0400

    testing an api with the ability to dynamically geet information

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return ('hello world')
 
+@app.route('/<string:name>')
+def name(name):
+    return (name)
+
 if __name__ == '__main__':
     app.run(debug=True)
